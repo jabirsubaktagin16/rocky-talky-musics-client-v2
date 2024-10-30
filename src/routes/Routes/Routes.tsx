d0e7150d37883834d554f60b7c2e45001d7843fa
Ahmad Subaktagin Jabir <subaktagin.jabir16@gmail.com>
2024-10-30 08:48:01 +0600

    Not Found Page, Sign In and Sign Up Page File Initialized

--- src/routes/Routes/Routes.tsx
+++ src/routes/Routes/Routes.tsx
@@ -1,15 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+import { NotFound } from "../../components/Shared/NotFound";
 import { Main } from "../../layouts/Main";
+import { SignIn } from "../../pages/Authentication/SignIn";
+import { SignUp } from "../../pages/Authentication/SignUp";
 import { Home } from "../../pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "sign-up",
+        element: <SignUp />,
       },
     ],
   },
